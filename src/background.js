fbaf13a351e1fcc8308aa641d80b4c348c0f7524
Lucas Bernardo <lucas.bernardo@softplan.com.br>
2019-06-12 09:52:56 -0300

    Update dependencies

--- src/background.js
+++ src/background.js
@@ -7,16 +7,20 @@ import images from './data.js';
   // Shuffle
   urls.sort(() => Math.random() - 0.5);
 
+  const circular = (array) => {
+    let current = 0;
+
+    return () => array[current++ % array.length];
+  };
+
+  const next = circular(urls);
+
   chrome.webRequest.onBeforeRequest.addListener(({ url }) => {
 
-    if (urls.includes(url)) {
-      return { redirectUrl: url };
+    if (!urls.includes(url)) {
+      const redirectUrl = next();
+      return { redirectUrl };
     }
-
-    const redirectUrl = urls.shift();
-    images.push(redirectUrl);
-
-    return { redirectUrl };
   },
   { urls: ["<all_urls>"], types: ['image'] },
   ["blocking"]);
